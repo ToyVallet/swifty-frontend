b2f663f02969f5ae6dda3b81022bcea011fece39
JUNSEOK <kd02109@gmail.com>
2024-06-06 15:27:47 +0900

    feat : user 조회 관련 api 연결 및 type 생성

--- apps/root-admin/src/app/(dashboard)/user/page.tsx
+++ apps/root-admin/src/app/(dashboard)/user/page.tsx
@@ -1,6 +1,9 @@
+import { customFetch } from "@/app/api";
+import { UserApi } from "@/app/types/user";
+import { API_CLIENT } from "@/constant";
+
 export default async function Page() {
-  const data = await (await fetch("https://dpi.swifty.kr/admin/host", {
-    credentials: "include",
-  })).json()
+  const data = await customFetch<UserApi>(API_CLIENT.host(), {cache: "no-cache"})
+
   return <div></div>;
 }
---
+++ apps/root-admin/src/app/types/user.ts
@@ -0,0 +1,28 @@
+type UserRole = "USER" | "GUEST" | "CLIENT" | "SUB_MANAGER" | "MANAGER" | "ADMIN" | "BANNED"
+
+type UserStatus = "ACTIVE" | "WITHDRAWL" | "PAUSE" | "BANNED"
+
+type UserGender = "OLD_MALE" | "OLE_FEMALE" | "YOUNG_MALE" | "YOUNG_FEMALE"
+
+export interface User {
+    userSubId: string
+    name: string
+    phoneNumber:string
+    userFormId: string
+    gender: UserGender
+    userRole: UserRole
+    enrolled: "FORM" | "KAKAO"
+    status: UserStatus
+}
+
+
+export interface UserApi{
+    content: User[]
+    hasNext: boolean
+    totalPages: number
+    totalElements: number
+    page: number
+    size: number
+    first: boolean
+    last: boolean
+}
