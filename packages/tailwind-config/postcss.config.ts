0715edf92bb75fde2231c8e0c9c1db872571505d
이호연 <ho991217@kakao.com>
2024-06-07 14:00:59 +0900

    chore: 공용 tailwind config 사용

--- packages/tailwind-config/postcss.config.ts
+++
@@ -1,9 +0,0 @@
-import config from './tailwind.config';
-
-export default {
-  plugins: {
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: { config },
-    autoprefixer: {},
-  },
-};
