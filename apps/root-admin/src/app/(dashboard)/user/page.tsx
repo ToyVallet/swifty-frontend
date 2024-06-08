e6656d3121ceef7ded1a8c49e2774f395ab62f57
JUNSEOK <kd02109@gmail.com>
2024-06-08 15:39:02 +0900

    feat: 쿠키 삽입 함수 생성

--- apps/root-admin/src/app/(dashboard)/user/page.tsx
+++ apps/root-admin/src/app/(dashboard)/user/page.tsx
@@ -1,14 +1,17 @@
 import { Table } from '@components/user';
 import { API_CLIENT } from '@lib/constant/api';
+import InsertCookieInRequest from '@lib/util/insert-cookie-in-request';
 import { customFetch } from '@swifty/shared-lib';
 import type { UserApi } from '@type/user';
 
 import styles from './user.module.css';
 
 export default async function Page() {
-  const data = await customFetch<UserApi>(API_CLIENT.hosts(), {
+  const requestOption = await InsertCookieInRequest({
     cache: 'no-cache',
   });
+
+  const data = await customFetch<UserApi>(API_CLIENT.users(), requestOption);
   return (
     <main>
       <h2 className={styles.heading}>계정 관리</h2>
---
+++ apps/root-admin/src/app/lib/util/insert-cookie-in-request.ts
@@ -0,0 +1,16 @@
+import { getCookieValue } from '@app/lib/cookies';
+import { COOKIE_KEYS, isServer } from '@swifty/shared-lib';
+
+export default async function InsertCookieInRequest(option: RequestInit) {
+  if (isServer()) {
+    const access = await getCookieValue(COOKIE_KEYS.accessToken);
+    const refresh = await getCookieValue(COOKIE_KEYS.refreshToken);
+    return {
+      headers: {
+        Cookie: `${COOKIE_KEYS.accessToken}=${access}; ${COOKIE_KEYS.refreshToken}=${refresh};`,
+      },
+      ...option,
+    };
+  }
+  return option;
+}
