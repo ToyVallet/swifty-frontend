cbd02b0a098ab75cb211839ec56469986c67ea99
이호연 <ho991217@kakao.com>
2024-06-09 20:41:07 +0900

    chore: customFetch를 서버에서도 쿠키를 담아보낼 수 있도록 변경

---
+++ packages/shared-lib/src/utils/server/get-serverside-cookies.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export default async function getServerSideCookies(): Promise<string> {
+  const allCookies = cookies().getAll();
+  const stringified = Object.entries(allCookies).reduce(
+    (acc, [, { name, value }]) => {
+      const cookie = `${name}=${value}`;
+      return acc + (acc ? '; ' : '') + cookie;
+    },
+    '',
+  );
+  return stringified;
+}
