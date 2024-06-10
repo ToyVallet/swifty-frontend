e9eea43f8fea306574b234f8e4db65350a545dbc
이호연 <ho991217@kakao.com>
2024-06-10 14:33:00 +0900

    feat: if 컴포넌트 추가

---
+++ packages/ui/src/lib/utils/if.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from 'react';
+
+export default function If({
+  condition,
+  children,
+}: PropsWithChildren<{ condition: boolean }>) {
+  return condition ? children : null;
+}
