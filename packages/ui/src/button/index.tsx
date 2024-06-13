699bc0429a592f92c195df4eb7e220767df76c1f
이호연 <ho991217@kakao.com>
2024-06-13 10:49:59 +0900

    chore: 버튼 글자색상 변경

--- packages/ui/src/button/index.tsx
+++ packages/ui/src/button/index.tsx
@@ -14,9 +14,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        default: 'bg-primary text-white hover:bg-primary/90',
+        destructive: 'bg-destructive text-white hover:bg-destructive/90',
         outlined: 'bg-transparent border border-white text-white',
         white: 'bg-white',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
