dbe2786778ac5241e48089492092b7ff49d3b3dd
JUNSEOK <kd02109@gmail.com>
2024-06-30 13:47:25 +0900

    remove: 사용하지 않는 constant, api 코드 삭제

--- apps/user-web/src/lib/types/festival.ts
+++ apps/user-web/src/lib/types/festival.ts
@@ -1,25 +1,5 @@
 import type { LineUp } from '@lib/types/line-up';
 
-export type FestivalImage = {
-  file: string;
-  originalFileName: string;
-  mimeType: string;
-  type: string;
-};
-
-export type FestivalInfo = {
-  id: string;
-  name: string;
-  addr: string;
-  startdate: string;
-  enddate: string;
-  festivalStatus: 'BEFORE' | 'PENDING' | 'AFTER';
-  description: string;
-  festivalFiles?: FestivalImage[];
-  festivalimage?: string;
-  url: string;
-};
-
 export type Festival_status = 'all' | 'available' | 'active' | 'end' | 'before';
 
 export type Festival = {
--- apps/user-web/src/lib/types/index.ts
+++ apps/user-web/src/lib/types/index.ts
@@ -1,3 +1,2 @@
-export { type FestivalImage, type FestivalInfo } from './festival';
 export * from './locale';
 export * from './line-up';
