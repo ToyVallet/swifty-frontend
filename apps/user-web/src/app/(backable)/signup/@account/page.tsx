399718fdd4fcf2210e6b3dbde23e0e3022727a66
JUNSEOK <kd02109@gmail.com>
2024-07-08 22:10:38 +0900

    feat: @account 적용

--- apps/user-web/src/app/(backable)/signup/@account/page.tsx
+++ apps/user-web/src/app/(backable)/signup/@account/page.tsx
@@ -1,3 +1,24 @@
+'use client';
+
+import type { FormValues } from '@app/(backable)/signup/schema';
+import IdCheckInput from '@components/signup/id-check-input';
+import { FormControl, FormField, FormItem, FormMessage } from '@swifty/ui';
+import { useFormContext } from 'react-hook-form';
+
 export default function AccountPage() {
-  return <div>asd</div>;
+  const form = useFormContext<FormValues>();
+  return (
+    <FormField
+      control={form.control}
+      name="id"
+      render={({ field }) => (
+        <FormItem>
+          <FormControl>
+            <IdCheckInput {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 }
