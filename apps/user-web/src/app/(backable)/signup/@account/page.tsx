53d3bc27cdc1941e17035b704c614ea72e19ff13
이호연 <ho991217@kakao.com>
2024-07-09 03:29:55 +0900

    feat: 전체 과정 완성

--- apps/user-web/src/app/(backable)/signup/@account/page.tsx
+++ apps/user-web/src/app/(backable)/signup/@account/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import { FixedBottomCTA } from '@components/common';
-import { Id, Password } from '@components/signup/account';
+import { Funnel } from '@components/signup';
+import { Id, Password, PasswordConfirm } from '@components/signup/account';
+import { type NonEmptyArray } from '@swifty/shared-lib';
 import { useContext } from 'react';
 
-import { StepContext } from '../context';
+import { type Step, StepContext, steps } from '../context';
+
+type StepType = Readonly<NonEmptyArray<Step>>;
 
 export default function AccountPage() {
-  const { nextStep } = useContext(StepContext);
+  const { nextStep, currentStep } = useContext(StepContext);
 
   const onNext = () => {
     nextStep();
@@ -16,8 +20,17 @@ export default function AccountPage() {
   return (
     <>
       <section className="flex flex-col gap-5">
-        <Password />
-        <Id />
+        <Funnel steps={steps} step={currentStep}>
+          <Funnel.Step<StepType> step="비밀번호를 한번 더 입력해주세요">
+            <PasswordConfirm />
+          </Funnel.Step>
+          <Funnel.Step<StepType> step="사용하실 비밀번호를 입력해주세요">
+            <Password />
+          </Funnel.Step>
+          <Funnel.Step<StepType> step="사용하실 아이디를 입력해주세요">
+            <Id />
+          </Funnel.Step>
+        </Funnel>
       </section>
       <FixedBottomCTA type="button" onClick={onNext}>
         다음
--- apps/user-web/src/components/signup/account/password-confirm.tsx
+++ apps/user-web/src/components/signup/account/password-confirm.tsx
@@ -1,15 +1,15 @@
 import { FormField, Input } from '@swifty/ui';
 
-export default function Password() {
+export default function PasswordConfirm() {
   return (
     <FormField
-      name="password"
+      name="passwordConfirm"
       render={({ field }) => (
         <Input
           {...field}
           type="password"
-          label="비밀번호"
-          placeholder="영문, 특수문자 포함 8자 이상"
+          label="비밀번호 확인"
+          placeholder="비밀번호를 한번 더 입력해주세요"
           autoComplete="new-password"
         />
       )}
