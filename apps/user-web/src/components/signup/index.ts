0d1be57d8f4ba70964d19aabe993315e57c99194
JUNSEOK <kd02109@gmail.com>
2024-07-08 22:14:27 +0900

    chore: better export

--- apps/user-web/src/components/signup/index.ts
+++ apps/user-web/src/components/signup/index.ts
@@ -1,3 +1,5 @@
 export { default as Funnel } from './funnel';
 export { default as Header } from './header';
+export { default as PhoneInput } from './phone-input';
+export { default as IdCheckInput } from './id-check-input';
 export * from './header';
