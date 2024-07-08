fff723d757284d7b9f57084b62520927af6c78ad
이호연 <ho991217@kakao.com>
2024-07-08 18:48:36 +0900

    feat: 라디오 버튼 그룹 추가

--- apps/user-web/src/app/(backable)/signup/@identification/page.tsx
+++ apps/user-web/src/app/(backable)/signup/@identification/page.tsx
@@ -1,3 +1,56 @@
-export default function Identification() { 
-  return <div>identification</div>
+'use client';
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  RadioGroup,
+  RadioGroupItem,
+} from '@swifty/ui';
+import { useFormContext } from 'react-hook-form';
+
+import { type FormValues } from '../schema';
+
+export default function Identification() {
+  const form = useFormContext<FormValues>();
+
+  return (
+    <FormField
+      control={form.control}
+      defaultValue="NATIVE"
+      name="nationality"
+      render={({ field }) => (
+        <FormItem className="space-y-3">
+          <FormControl>
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="flex w-full gap-5 items-center justify-between"
+            >
+              <FormItem className="flex w-full">
+                <FormControl>
+                  <RadioGroupItem value="NATIVE">
+                    <FormLabel className="font-bold text-16 z-10">
+                      내국인
+                    </FormLabel>
+                  </RadioGroupItem>
+                </FormControl>
+              </FormItem>
+
+              <FormItem className="flex w-full">
+                <FormControl>
+                  <RadioGroupItem value="FOREIGNER">
+                    <FormLabel className="font-bold text-16 z-10">
+                      외국인
+                    </FormLabel>
+                  </RadioGroupItem>
+                </FormControl>
+              </FormItem>
+            </RadioGroup>
+          </FormControl>
+        </FormItem>
+      )}
+    ></FormField>
+  );
 }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,14 @@
 export { default as Button } from './button';
 export { default as Input } from './input';
-export * from './form';
+export { RadioGroup, RadioGroupItem } from './radio-group';
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from './form';
 export * from './lib';
