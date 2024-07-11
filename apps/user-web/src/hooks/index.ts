5498c7cdc348a68bc011e7329031361f6ead5769
JUNSEOK <kd02109@gmail.com>
2024-07-11 10:50:43 +0900

    chore: 파일 이름 수정

--- apps/user-web/src/hooks/index.ts
+++ apps/user-web/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useBottomSheet } from './use-bottom-sheet';
 export { default as useDimensions } from './use-dimensions';
 export { default as useClientSideToken } from './use-client-side-token';
+export { default as useInterval } from './use-interval';
