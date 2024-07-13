01aebb4c1e2886d32a1181afdfe45a9be03fee9c
JUNSEOK <kd02109@gmail.com>
2024-07-13 13:41:03 +0900

    feat: useDebounce

---
+++ apps/user-web/src/hooks/use-debounce.ts
@@ -0,0 +1,31 @@
+'use client';
+
+// Cleanup the previous timeout on re-render
+import { useEffect, useRef } from 'react';
+
+export default function useDebounce<F extends (...args: any[]) => any>(
+  callback: F,
+  delay: number,
+) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  const debouncedCallback = (...args: any[]) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+
+  return debouncedCallback;
+}
