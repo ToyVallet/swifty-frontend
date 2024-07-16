87c82b39c45ae8e15a380de85f0a855878feeb21
JUNSEOK <kd02109@gmail.com>
2024-07-16 09:58:48 +0900

    feat: 인증번호 구현

--- apps/user-web/src/app/(backable)/change-password/@password/page.tsx
+++ apps/user-web/src/app/(backable)/change-password/@password/page.tsx
@@ -1,3 +1,31 @@
+'use client';
+
+import {
+  FindPasswordContext,
+  steps,
+} from '@app/(backable)/change-password/context';
+import { FixedBottomCTA } from '@components/common';
+import { Funnel } from '@components/signup';
+import type { StepType } from '@components/signup/funnel';
+import { PhoneNumber } from '@components/signup/identification';
+import { useContext } from 'react';
+
 export default function PasswordPage() {
-  return;
+  const { currentStep, nextStep } = useContext(FindPasswordContext);
+  return (
+    <>
+      <section>
+        <Funnel step={currentStep} steps={steps}>
+          <Funnel.Step<StepType> step="휴대폰 번호를 입력하세요">
+            <PhoneNumber />
+          </Funnel.Step>
+
+          <Funnel.Step<StepType> step="인증번호를 입력하세요">
+            <PhoneNumber />
+          </Funnel.Step>
+        </Funnel>
+      </section>
+      <FixedBottomCTA>다음</FixedBottomCTA>
+    </>
+  );
 }
--- apps/user-web/src/app/(backable)/change-password/context.ts
+++ apps/user-web/src/app/(backable)/change-password/context.ts
@@ -1,4 +1,7 @@
+'use client';
+
 import { createContext } from 'react';
+import { TypeOf } from 'zod';
 
 export const phoneSteps = [
   '휴대폰 번호를 입력하세요',
@@ -15,6 +18,15 @@ export const steps = [
   ...passwordSteps,
   '비밀번호가 재설정 되었습니다.',
 ] as const;
+
+export const stepsWithForm = {
+  '휴대폰 번호를 입력하세요': 'phoneNumber',
+  '인증번호를 입력하세요': 'smsCode',
+  '현재 비밀번호를 입력하세요': 'currentPassword',
+  '새 비밀번호를 입력하세요': 'newPassword',
+  '비밀번호를 확인해주세요': 'confirmNewPassword',
+  '비밀번호가 재설정 되었습니다.': '',
+};
 
 export type FindPasswordStep = (typeof steps)[number];
 export const FindPasswordContext = createContext<{
