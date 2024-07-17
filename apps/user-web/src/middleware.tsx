d870900401437147bb2726b74c720d963103ab81
JUNSEOK <kd02109@gmail.com>
2024-07-17 20:30:28 +0900

    feat: 서버 컴포넌트 path 접근

--- apps/user-web/src/middleware.tsx
+++ apps/user-web/src/middleware.tsx
@@ -36,7 +36,15 @@ export async function middleware(request: NextRequest) {
     }
   }
 
-  return NextResponse.next();
+  // 서버 컴포넌트 headername
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-pathname', request.nextUrl.pathname);
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
 
 export const config = {
