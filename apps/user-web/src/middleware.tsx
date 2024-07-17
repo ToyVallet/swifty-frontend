f4091fe6c06dcad4390cda09232f785b57c382f8
JUNSEOK <kd02109@gmail.com>
2024-07-17 12:50:16 +0900

    feat: middleware 설정

---
+++ apps/user-web/src/middleware.tsx
@@ -0,0 +1,44 @@
+'use server';
+
+import { COOKIE_KEYS } from '@swifty/shared-lib';
+import { cookies } from 'next/headers';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const token = cookies().get(COOKIE_KEYS.accessToken);
+
+  const pathRequiredLogin = [
+    'univ-certification',
+    'mypage',
+    'ticketing',
+    'change-password',
+    'change-phone-number',
+  ] as const;
+
+  if (token) {
+    // 로그인 했을 경우 접근해서는 안되는 페이지
+    // login, signup,
+    if (url.pathname === '/login' || url.pathname === '/signup') {
+      url.pathname = '/';
+      return NextResponse.redirect(url);
+    }
+  } else {
+    // 로그인 하지 않았을 경우 접근해서는 안되는 페이지
+    // 학적 인증 univ-certification, 마이페이지 mypage, 티켓팅 festival/[id]/ticketing, change-password, change-phone-number
+    for (let i = 0; i < pathRequiredLogin.length; i += 1) {
+      const value = pathRequiredLogin[i] as (typeof pathRequiredLogin)[number];
+      if (url.pathname.includes(value)) {
+        url.pathname = '/login'; //로그인 페이지로 리다이렉트
+        return NextResponse.redirect(url);
+      }
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
