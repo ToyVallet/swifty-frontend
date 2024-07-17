d80bf1e8e11c3684266fb6d546d0a1df0904f338
JUNSEOK <kd02109@gmail.com>
2024-07-17 20:32:11 +0900

    feat: mypage Navigation 기능 분기 처리

--- apps/user-web/src/app/(backable)/mypage/layout.tsx
+++ apps/user-web/src/app/(backable)/mypage/layout.tsx
@@ -1,10 +1,18 @@
 import { Navigation } from '@components/common';
+import { headers } from 'next/headers';
 import { type PropsWithChildren } from 'react';
 
 export default function SearchLayout({ children }: PropsWithChildren) {
+  const headersList = headers();
+  const path = headersList.get('x-pathname') || '';
+
   return (
     <>
-      <Navigation variant="back" title="마이페이지" />
+      {path.includes('info') ? (
+        <Navigation variant="back" title="계정 관리" />
+      ) : (
+        <Navigation variant="back" title="마이페이지" />
+      )}
       {children}
     </>
   );
