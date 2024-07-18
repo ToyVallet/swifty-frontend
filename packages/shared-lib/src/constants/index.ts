d9d31de5b864f3ac260e64402393a76aa3db9f77
ho991217 <ho991217@kakao.com>
2024-07-18 04:49:10 +0000

    feat: 요청 위치에 따라 엔드포인트 구분하도록 설정

--- packages/shared-lib/src/constants/index.ts
+++ packages/shared-lib/src/constants/index.ts
@@ -4,6 +4,11 @@
 export const SERVER_URL: string = 'http://localhost:8080';
 
 /**
+ * @description 서버 URL
+ */
+export const SERVER_EXTERNAL_URL: string = 'https://swifty.kr/api';
+
+/**
  * @description 쿠키 키값
  */
 export const COOKIE_KEYS = {
