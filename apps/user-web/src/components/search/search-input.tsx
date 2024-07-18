3c0d83b7b25f4d0295d12cb4cd57cbbd6a5863c4
이호연 <ho991217@kakao.com>
2024-07-18 18:02:11 +0900

    feat: 검색기능 완성

---
+++ apps/user-web/src/components/search/search-input.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import Magnifier from '@icons/magnifier.svg';
+import { customFetch } from '@swifty/shared-lib';
+import { If, Input, transition } from '@swifty/ui';
+import { useDebounce } from '@toss/react';
+import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/navigation';
+import { use, useEffect, useState } from 'react';
+
+export default function SearchInput() {
+  const router = useRouter();
+  const [searchValue, setSearchValue] = useState('');
+  const [autocompleteList, setAutocompleteList] = useState<string[]>([]);
+
+  const autocomplete = useDebounce(async () => {
+    const list = await customFetch<string[]>(
+      `/festival/auto-complete?keyword=${searchValue}`,
+      {
+        method: 'GET',
+      },
+    );
+    setAutocompleteList(list);
+  }, 500);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearchValue(value);
+    if (value === '') {
+      setAutocompleteList([]);
+      return;
+    }
+    autocomplete();
+  };
+
+  const search = (value: string) => {
+    router.replace(`/search?keyword=${value}`);
+  };
+
+  useEffect(() => {
+    if (searchValue === '') {
+      setAutocompleteList([]);
+    }
+  }, [searchValue]);
+
+  return (
+    <div className="flex flex-col gap-2.5 absolute top-[77px] left-5 right-5">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          search(searchValue);
+        }}
+      >
+        <Input
+          name="search"
+          type="text"
+          label="검색"
+          placeholder="학교명을를 입력하세요"
+          value={searchValue}
+          onChange={handleChange}
+        />
+      </form>
+      <AnimatePresence>
+        <If condition={autocompleteList.length > 0}>
+          <motion.div
+            initial={{
+              opacity: 0,
+              scale: 0.95,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            exit={{
+              opacity: 0,
+              scale: 0.95,
+            }}
+            transition={transition}
+            className="text-white bg-swifty-color-800 rounded-xl py-3 flex flex-col items-center justify-center w-full"
+          >
+            {autocompleteList.map((item) => (
+              <button
+                key={item}
+                className="py-3 px-5 w-full flex items-center justify-between"
+                onClick={() => {
+                  setSearchValue(item);
+                  setAutocompleteList([]);
+                  search(item);
+                }}
+              >
+                <span>{item}</span>
+                <Magnifier />
+              </button>
+            ))}
+          </motion.div>
+        </If>
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ apps/user-web/src/components/search/search-result/item.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { ImageWithFallback } from '@components/common';
+import FalBackImg from '@images/fallback-festival.png';
+import { formatDateRange } from '@swifty/shared-lib';
+import { transition } from '@swifty/ui';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import NextLink from 'next/link';
+
+import { type SearchResult } from '.';
+
+const Link = motion(NextLink);
+
+function Item({ item }: { item: SearchResult }) {
+  return (
+    <Link
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition}
+      whileTap={{ scale: 0.98, backgroundColor: '#1a1a1a' }}
+      href={`/festival/${item.id}`}
+      className="text-white bg-black flex w-full items-center gap-[30px] p-2.5 rounded-xl"
+    >
+      <ImageWithFallback
+        fallback={FalBackImg}
+        src={item.thumbnailImagePath}
+        alt={item.name}
+        width={70}
+        height={70}
+        className="object-cover w-[70px] h-[70px] rounded-full"
+      />
+      <div className="flex flex-col items-start justify-center">
+        <h2 className="text-16 font-semibold">{item.name}</h2>
+        <p className="text-16 font-medium">{item.description}</p>
+        <p className="text-12 font-normal text-swifty-color-400 mt-1">
+          {formatDateRange(item.startDate, item.endDate)}
+        </p>
+      </div>
+    </Link>
+  );
+}
+
+export default Item;
