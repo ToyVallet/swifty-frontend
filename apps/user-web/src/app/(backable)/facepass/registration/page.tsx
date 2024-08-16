ff592e971a31dd3601b48919e37089b16473a1d4
JUNSEOK <kd02109@gmail.com>
2024-08-16 20:56:31 +0900

    chore: facepass build dynamic 적용

--- apps/user-web/src/app/(backable)/facepass/registration/page.tsx
+++ apps/user-web/src/app/(backable)/facepass/registration/page.tsx
@@ -1,5 +1,13 @@
-import { Landmark } from '@components/facepass';
+'use client';
+
+//import { Landmark } from '@components/facepass';
+import dynamic from 'next/dynamic';
+
+const LandMark = dynamic(
+  () => import('@components/facepass').then((mod) => mod.Landmark),
+  { ssr: false },
+);
 
 export default function FacePassRegistrationPage() {
-  return <Landmark />;
+  return <LandMark />;
 }
