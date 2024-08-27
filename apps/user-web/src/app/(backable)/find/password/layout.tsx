be899f6231f32ab711c8ab895052bd23b190189b
JUNSEOK <kd02109@gmail.com>
2024-08-27 16:11:18 +0900

    fix: find id pw navigation css

--- apps/user-web/src/app/(backable)/find/password/layout.tsx
+++ apps/user-web/src/app/(backable)/find/password/layout.tsx
@@ -44,7 +44,7 @@ export default function Page({ id, phone, password, complete }: Props) {
   };
   return (
     <>
-      <Navigation title="비밀번호 찾기" />
+      <Navigation title="비밀번호 찾기" bg="transparent" />
       <FindPasswordContext.Provider value={{ currentStep, nextStep }}>
         <Main className="relative pb-20 scrollbar-hide px-5 mt-[47px]">
           <Header>{currentStep}</Header>
