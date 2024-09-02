81b54617774f1c5b7f0f25add24772c5726aa3b4
JUNSEOK <kd02109@gmail.com>
2024-09-02 21:38:47 +0900

    feat: desktop 디자인 width 크기 조정

--- apps/user-web/src/app/layout.tsx
+++ apps/user-web/src/app/layout.tsx
@@ -47,7 +47,7 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko">
-      <body className="font-Pretendard scrollbar-hide bg-swifty-color-100 dark:bg-swifty-color-dark-bg text-black dark:text-white">
+      <body className="font-Pretendard scrollbar-hide bg-swifty-color-100 dark:bg-swifty-color-dark-bg text-black dark:text-white mx-auto max-w-[640px] relative">
         <Provider>
           <ThemeProvider
             attribute="class"
