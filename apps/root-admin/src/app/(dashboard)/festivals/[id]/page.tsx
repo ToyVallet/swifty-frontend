d2715c093950cd7f84434e9b81ccd3b935c57f2f
JUNSEOK <kd02109@gmail.com>
2024-09-02 18:07:37 +0900

    feat: 철자 수정

--- apps/root-admin/src/app/(dashboard)/festivals/[id]/page.tsx
+++ apps/root-admin/src/app/(dashboard)/festivals/[id]/page.tsx
@@ -8,6 +8,7 @@ import {
 import type { Params } from '@swifty/shared-lib';
 import type { FestivalDetail } from '@type';
 import { Descriptions, Flex } from 'antd';
+import dayjs from 'dayjs';
 
 import { getDetailFestival } from './get-detail-festival';
 import styles from './page.module.css';
@@ -34,8 +35,12 @@ export default async function Page({ params: { id } }: Params<{ id: string }>) {
   ).map((item) => ({
     key: item,
     label: item,
-    children: adminFestivalInfoResponse[item],
+    children:
+      item === 'description' || item === 'addr'
+        ? item
+        : dayjs(adminFestivalInfoResponse[item]).format('YY-MM-DD HH:mm:ss'),
   }));
+
   return (
     <main className={styles.main}>
       <Flex align="center" gap={'1rem'} className={styles.headerWrapper}>
